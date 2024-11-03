238ace8367a3db63593564bb70e538436eeb44fb
kdollmayer <karldollmayer@gmail.com>
2024-11-03 15:17:27 +0100

    fibonacci

---
+++ fibonacci_apicall.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func fibonacci(n int) int {
+	if n <= 1 {
+		return n
+	}
+	return fibonacci(n-1) + fibonacci(n-2)
+}
+
+func callAPIWithRetries(maxRetries int) {
+	attempt := 0
+	for attempt = 1; attempt <= maxRetries; attempt++ {
+		success := makeAPICall(attempt)
+		if success {
+			fmt.Println("API call successful!")
+			return
+		}
+		delay := fibonacci(attempt) * 100
+		fmt.Printf("Retry attemp %d failed. Retrying in %d ms \n", attempt, delay)
+		time.Sleep(time.Duration(delay) * time.Millisecond)
+
+	}
+
+	fmt.Println("All retries failed")
+}
+
+func makeAPICall(n int) bool {
+	return n == 5
+}
+func main() {
+	callAPIWithRetries(6)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/KDollmayer/golang_fibonacci_exercise
+
+go 1.23.2
